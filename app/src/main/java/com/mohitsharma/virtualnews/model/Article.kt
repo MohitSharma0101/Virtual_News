53b8c1767d7c569e20f0e7f4758d1c3b63ac689f
MohitSharma0101 <mohitsharma.2cse23@jecrc.ac.in>
2020-11-01 00:23:18 +0530

    Article.kt class updated

--- app/src/main/java/com/mohitsharma/virtualnews/model/Article.kt
+++ app/src/main/java/com/mohitsharma/virtualnews/model/Article.kt
@@ -1,17 +1,20 @@
 package com.mohitsharma.virtualnews.model
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
+
 @Entity(
-        tableName = "articles"
+        tableName = "articles",
+        indices = [Index(value = ["title", "publishedAt"],
+        unique = true)]
 )
 data class Article(
         @PrimaryKey(autoGenerate = true)
         var id : Int? = null,
-        var isSaved: Boolean? = false,
         var author: String,
-        val content: String,
+        val content: String?,
         val description: String,
         var publishedAt: String,
         val source: Source,
